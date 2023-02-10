bc8d2a1ec1a61540f68cc609983f3204e42de416
Akzhol <suborovakzhol7@gmail.com>
2023-02-10 11:08:48 +0600

    First commit

---
+++ vpc/output.tf
@@ -0,0 +1,21 @@
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+  
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public[*].id
+  
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private[*].id
+}
+
+output "project" {
+  value = var.project
+  
+}
+
+
