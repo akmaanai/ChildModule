f1bdad7601d7edb028cbd709032b1200f7e2623a
Akzhol <73769876+AkzholS7@users.noreply.github.com>
2023-06-03 23:28:56 +0600

    Update node_groups.tf
    changed from private_subnet to public_subnet

--- eks/node_groups.tf
+++ eks/node_groups.tf
@@ -4,7 +4,7 @@ resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = var.project
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = var.private_subnet
+  subnet_ids      = var.public_subnet
 
   scaling_config {
     desired_size = each.value.desired_size
